3323c28cb7c10f0e056853c8ec6e960c40e3deb4
Nate West <natescott.west@gmail.com>
2016-03-04 09:59:37 -0800

    Who needs a backend?

--- src/components/wizardmon.tsx
+++ src/components/wizardmon.tsx
@@ -1,20 +1,16 @@
 import * as React from "react";
 import HungerAction from "../actions/hunger_actions.ts";
-import { getLastFed, hungerStore } from "../stores/hunger_store.ts";
+import hungerStore from "../stores/hunger_store.ts";
 
 interface WizardmonProps {}
 
 export interface WizardmonState {
-  username: string;
-  name: string;
   last_fed: Date;
-  id: number;
 }
 
 class Wizardmon extends React.Component<WizardmonProps, WizardmonState> {
   constructor(props, context) {
     super(props, context);
-    getLastFed();
     this.state = hungerStore.getState();
     hungerStore.subscribe(() => {
       this.setState(hungerStore.getState());
--- src/stores/hunger_store.ts
+++ src/stores/hunger_store.ts
@@ -13,17 +13,6 @@ export interface HungerStoreAction {
   response: WizardmonState;
 };
 
-export function getLastFed(): void {
-  getJSON("http://localhost:4000/digimon/niftyn8", (response) => {
-    setLastFed(response);
-  });
-}
-
-function feed(data: WizardmonState & any): void {
-  patchJSON("http://localhost:4000/digimon/niftyn8", data, (response): void => {
-    setLastFed(response);
-  });
-}
 
 function setLastFed(response) {
   _.merge(response.data, {last_fed: Date.parse(response.data.last_fed)});
@@ -34,10 +23,8 @@ export const hunger = (state: WizardmonState, action: HungerStoreAction) => {
   switch (action.type) {
   case HungerAction.Feed:
     let newState = _.merge(state, {last_fed: new Date()});
-    feed({ "digimon": newState });
-    return state;
+    return newState;
   case HungerAction.Update:
-    getLastFed();
     return state;
   case HungerAction.SetLastFed:
     return action.response;
