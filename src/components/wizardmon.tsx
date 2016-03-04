75e04315a171c3470bfc9605d7f2e6c39a5d9889
Nate West <natescott.west@gmail.com>
2016-03-03 19:39:53 -0800

    Weak feeding...
    Just messing with Redux right now...
    
    Will wire it up to the backend soon....

--- src/components/wizardmon.tsx
+++ src/components/wizardmon.tsx
@@ -1,23 +1,59 @@
+import * as _ from "lodash";
 import * as React from "react";
 import { createStore } from "redux";
 
-interface WizardmonProps {
-  status: String;
+enum HungerAction {
+  Feed,
+  NotFeed
 }
 
-interface WizardmonState {}
+class HungerStoreAction {
+  type: HungerAction;
+};
+
+const hunger = (state: WizardmonState = {hunger: 0}, action: HungerStoreAction) => {
+  switch (action.type) {
+  case HungerAction.Feed:
+    return _.merge(state, {hunger: state.hunger - 1});
+  case HungerAction.NotFeed:
+    return _.merge(state, {hunger: state.hunger + 1});
+  default:
+    return state;
+  }
+};
+
+const hungerStore = createStore(hunger);
+
+interface WizardmonProps {}
+
+interface WizardmonState {
+  hunger: number;
+}
 
 class Wizardmon extends React.Component<WizardmonProps, WizardmonState> {
   constructor(props, context) {
     super(props, context);
-    this.state = {};
+    this.state = hungerStore.getState();
+    hungerStore.subscribe(() => {
+      this.setState(hungerStore.getState());
+    });
+  }
+
+  feed(): void {
+    hungerStore.dispatch({ type: HungerAction.Feed });
+  }
+
+  unfeed(): void {
+    hungerStore.dispatch({ type: HungerAction.NotFeed });
   }
 
   render() {
     return(
       <div>
         <img src="assets/images/wizardmon.gif"></img>
-        <p className="wizardmon-status">{this.props.status}</p>
+        <p className="wizardmon-hunger">Hunger: {this.state.hunger}</p>
+        <button onClick={this.feed}>Feed</button>
+        <button onClick={this.unfeed}>Unfeed</button>
       </div>
     );
   }
--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,4 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Wizardmon from "./components/wizardmon.tsx";
 
-ReactDOM.render(<Wizardmon status="hungry"></Wizardmon>, document.getElementById("app"));
+ReactDOM.render(<Wizardmon></Wizardmon>, document.getElementById("app"));
